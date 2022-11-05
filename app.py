1d40116e2ef24a4ffd2c7bd07778c9b45a07ff86
maritriachini <mtriachini@hotmail.com>
2022-11-05 12:56:19 +0000

    atualizado

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "ola mundo"
+@app.route("/health")
+def health():
+    return "OK"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=(os.environ.get("APP_PORT")), debug=True)
