029fbdd7ffc04b94b23aa3eb08231bf1d848d8d7
maritriachini <mtriachini@hotmail.com>
2022-11-05 15:14:12 +0000

    atualizado services

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ import os
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello_world():
+    return "ola mundo"
+
 @app.route("/health")
 def health():
-    return "OK"
+        return "OK"
+count=0
 
+@app.route("/counter")
+def counter():
+    global count
+    count+=1
+    return str(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=(os.environ.get("APP_PORT")), debug=True)
