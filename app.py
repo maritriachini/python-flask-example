c44f779c3337bf62396b345f44bfb52c8e62f6e8
maritriachini <mtriachini@hotmail.com>
2022-11-09 00:55:26 +0000

    atualização de versão/nodeport/loadbalancer

--- app.py
+++ app.py
@@ -16,7 +16,13 @@ count=0
 def counter():
     global count
     count+=1
-    return str(count)
+    return str(count) + "\n"
+
+#app-flask:1.0.0
+#docker build . -t app-flask:2.0.0
+@app.route("/version")
+def counter_route():
+    return "2.0.0"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=(os.environ.get("APP_PORT")), debug=True)
